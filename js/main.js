075d9bcefe6754bbe82dff5658ee2a55eefaa4d2
Dinuka Ekanayake <dilshandinuka@gmail.com>
2021-12-20 02:28:33 +0530

    Upgrade v2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
  AOS.init({
- 	duration: 100,
+ 	duration: 800,
  	easing: 'slide',
  	once: false
  });
@@ -8,6 +8,7 @@ jQuery(document).ready(function($) {
 
 	"use strict";
 
+	
 
 	var siteMenuClone = function() {
 
@@ -122,7 +123,40 @@ jQuery(document).ready(function($) {
 	// siteSliderRange();
 
 
-	
+	var siteMagnificPopup = function() {
+		$('.image-popup').magnificPopup({
+	    type: 'image',
+	    closeOnContentClick: true,
+	    closeBtnInside: false,
+	    fixedContentPos: true,
+	    mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+	     gallery: {
+	      enabled: true,
+	      navigateByImgClick: true,
+	      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+	    },
+	    image: {
+	      verticalFit: true
+	    },
+	    zoom: {
+	      enabled: true,
+	      duration: 300 // don't foget to change the duration also in CSS
+	    }
+	  });
+
+	  $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+	    disableOn: 700,
+	    type: 'iframe',
+	    mainClass: 'mfp-fade',
+	    removalDelay: 160,
+	    preloader: false,
+
+	    fixedContentPos: false
+	  });
+	};
+	siteMagnificPopup();
+
+
 	var siteCarousel = function () {
 		if ( $('.nonloop-block-13').length > 0 ) {
 			$('.nonloop-block-13').owlCarousel({
@@ -162,15 +196,12 @@ jQuery(document).ready(function($) {
 	    loop: true,
 			stagePadding: 0,
 	    margin: 0,
-	    smartSpeed: 1000,
+	    dots: true,
 	    autoplay: true,
 	    pauseOnHover: false,
-	    autoHeight: true,
-	    nav: false,
+	    nav: true,
 	    navText: ['<span class="icon-keyboard_arrow_left">', '<span class="icon-keyboard_arrow_right">']
 	  });
-
-	  
 	};
 	siteCarousel();
 
@@ -184,9 +215,22 @@ jQuery(document).ready(function($) {
 	    scrollProperty: 'scroll'
 	  });
 	};
-	// siteStellar();
+	siteStellar();
 
-	
+	var siteCountDown = function() {
+
+		$('#date-countdown').countdown('2020/10/10', function(event) {
+		  var $this = $(this).html(event.strftime(''
+		    + '<span class="countdown-block"><span class="label">%w</span> weeks </span>'
+		    + '<span class="countdown-block"><span class="label">%d</span> days </span>'
+		    + '<span class="countdown-block"><span class="label">%H</span> hr </span>'
+		    + '<span class="countdown-block"><span class="label">%M</span> min </span>'
+		    + '<span class="countdown-block"><span class="label">%S</span> sec</span>'));
+		});
+				
+	};
+	siteCountDown();
+
 	var siteDatePicker = function() {
 
 		if ( $('.datepicker').length > 0 ) {
@@ -196,63 +240,30 @@ jQuery(document).ready(function($) {
 	};
 	siteDatePicker();
 
-	var siteSticky = function() {
-		$(".js-sticky-header").sticky({topSpacing:0});
-	};
-	siteSticky();
-
 	// navigation
   var OnePageNavigation = function() {
     var navToggler = $('.site-menu-toggle');
    	$("body").on("click", ".main-menu li a[href^='#'], .smoothscroll[href^='#'], .site-mobile-menu .site-nav-wrap li a", function(e) {
       e.preventDefault();
-
       var hash = this.hash;
-
-      $('html, body').animate({
-        'scrollTop': $(hash).offset().top
-      }, 600, 'easeInOutExpo', function(){
-        window.location.hash = hash;
-      });
+        $('html, body').animate({
+          'scrollTop': $(hash).offset().top
+        }, 800, 'swing', function(){
+          window.location.hash = hash;
+        });
 
     });
+
+    // $("#menu li a[href^='#']").on('click', function(e){
+    //   e.preventDefault();
+    //   navToggler.trigger('click');
+    // });
+
+    $('body').on('activate.bs.scrollspy', function () {
+      // console.log('nice');
+      // alert('yay');
+    })
   };
   OnePageNavigation();
 
-  var siteScroll = function() {
-
-  	
-
-  	$(window).scroll(function() {
-
-  		var st = $(this).scrollTop();
-
-  		if (st > 100) {
-  			$('.js-sticky-header').addClass('shrink');
-  		} else {
-  			$('.js-sticky-header').removeClass('shrink');
-  		}
-
-  	}) 
-
-  };
-  siteScroll();
-
-
-
-  $('.fancybox').on('click', function() {
-	  var visibleLinks = $('.fancybox');
-
-	  $.fancybox.open( visibleLinks, {}, visibleLinks.index( this ) );
-
-	  return false;
-	});
-
-});
-
-$('#back-to-top').on('click', function (e) {
-    e.preventDefault();
-    $('html,body').animate({
-        scrollTop: 0
-    }, 800);
 });
